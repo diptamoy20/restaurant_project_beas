c5053f28c630ca3f41eee05c5fe68058ae0b91c9
Tuhin-beas <tuhin.roy@beas.co.in>
2026-06-16 18:31:02 +0530

    improved the socket logic

--- backend/src/modules/deliveries/deliveries.gateway.ts
+++ backend/src/modules/deliveries/deliveries.gateway.ts
@@ -109,10 +109,10 @@ export class DeliveriesGateway implements OnGatewayConnection {
     try {
       const user = this.getSocketUser(client);
 
-      // if (user.role !== Role.DELIVERY_BOY) {
-      //   // throw new ForbiddenException('Only delivery boys can update live location');
-      //   throw new ForbiddenException(user.role);
-      // }
+      if (user.role !== Role.DELIVERY_BOY) {
+        throw new ForbiddenException('Only delivery boys can update live location');
+        // throw new ForbiddenException(user.role);
+      }
 
       const result = await this.deliveriesService.updateMyLiveLocation(user, payload);
       const data = {
@@ -149,9 +149,9 @@ export class DeliveriesGateway implements OnGatewayConnection {
       throw new UnauthorizedException('Invalid token type');
     }
 
-    if (payload.role !== Role.DELIVERY_BOY) {
-      throw new ForbiddenException('Only delivery boys can connect to tracking socket');
-    }
+    // if (payload.role !== Role.DELIVERY_BOY) {
+    //   throw new ForbiddenException('Only delivery boys can connect to tracking socket');
+    // }
 
     const userId = payload.sub ?? payload.userId;
 
